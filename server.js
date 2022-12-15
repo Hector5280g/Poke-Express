0e7cd61d73171c73ceb6aa4d27dabb17f5b4a5d9
Hector5280g <hectorgarcia342@yahoo.com>
2022-12-15 14:27:18 -0700

    edits and working

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-require('dotenv').config()
-const express = require("express");
-const app = express();
-const pokemon  = require("./models/pokemon");
-const mongoose = require("mongoose");
-app.set('view engine', 'jsx');
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+const express = require("express")
+const app = express()
+const Pokemon  = require("./models/pokemon")
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 app.engine('jsx', require('express-react-views').createEngine())
+app.set('view engine', 'jsx')
+app.use(express.static('styles'))
+app.use(express.urlencoded({ extended: false }))
 
-
-// Setup inputs for our connect function
-// Establish Connection
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.DATABASE_URI, {
     useNewUrlParser: true,
@@ -18,27 +21,66 @@ mongoose.connection.once('open', () => {
     console.log("connected to MongoDB");
 })
 
-
-
-// Index route
-app.get('/', (req, res) => {
-    res.render('Index', {
-        pokemon: pokemon
-    })
-});
-
-  // New route
-  app.get("/New", (req, res) => {
-    res.render("New",);
+// Index
+app.get("/", (req, res) => {
+    //find all fruits
+    Pokemon.find({}, (error, allPokemon)=>{
+      res.render('Index', {
+        Pokemon: allPokemon
+      })
+    }) 
   });
 
-//Show route
+// New
+app.get("/new", (req, res) => {
+    res.render('New')
+});
+
+// Post
+app.post('/pokemon', (req, res) => {
+    Pokemon.create(req.body, (error, createdPokemon) => {
+        res.redirect("/");
+    });
+})
+
+// Show
 app.get( '/pokemon/:id', (req, res) => {
-    res.render('Show', {
-        pokemon: pokemon[req.params.id]
+    Pokemon.findById(req.params.id, (err, foundPokemon) => {
+        res.render('Show', {
+            pokemon: foundPokemon
+        })
     })
 });
 
-// App listening
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
+// Edit
+app.get('/pokemon/:id/edit', (req, res)=> {
+    // finding pokemon by ID
+    // render an edit form
+    // pass in the pokemon data "payload"
+    Pokemon.findById(req.params.id, (err, foundPokemon) => {
+        res.render('Edit', {
+            pokemon: foundPokemon
+        })
+    })
+})
+// Update
+app.put('/pokemon/:id', (req, res) => {
+    // find the pokemon by ID and update
+    // redirect to the pokemon's show page
+    Pokemon.findByIdAndUpdate(req.params.id, req.body, (err, updatedPokemon) => {
+        console.log(updatedPokemon)
+        res.redirect(`/pokemon/${req.params.id}`)
+    })
+})
+
+// Delete
+app.delete('/pokemon/:id', (req, res)=>{
+    Pokemon.findByIdAndRemove(req.params.id, (err, deletedPokemon) => {
+        res.redirect('/')
+    })
+});
+
+// Port
+app.listen( 3000, () => {
+    console.log("listening")
+});
--- views/Index.jsx
+++ views/Index.jsx
@@ -1,39 +1,92 @@
 import React, { Component } from 'react'
 
-const headerStyle = {
-    color: '#ffffff',
+// HTML Styling
+const bodyStyling = {
+    margin: '0 auto',
+    textAlign: 'center',
+    backgroundImage: 'url(https://images.wallpapersden.com/image/download/pokemon-pikachu-art_a25tbW6UmZqaraWkpJRmbmdlrWZlbWU.jpg)',
+    marginTop: '1%',
+    width: '100vw',
+    height: '100vh',
+    overflow: 'hidden'
+};
+const navStyling = {
+    textAlign: 'center',
+  }
+const htmlStyling = {
+    margin: '0 auto',
+    textAlign: 'center',
+    backgroundImage: 'url(https://images.wallpapersden.com/image/download/pokemon-pikachu-art_a25tbW6UmZqaraWkpJRmbmdlrWZlbWU.jpg)',
+    width: '100vw',
+    height: '100vh'
+};
+// Base Styling
+  const myStyle = {
+        color: '#ffffff',
         background: 'red',
         padding: '8px',
         fontSize: '20px',
         borderRadius: '7px',
         fontFamily: 'monospace',
         textAlign: 'center',
-        maxWidth: '968px',
+        maxWidth: '1400px',
         margin: '0 auto',
-}
-const list = {
-    listStyle: 'none',
-    padding: '30px',
-    borderRadius: '4px',
-    textAlign: 'center'
-};
-export default class Index extends Component {
-    render() {
-      const {pokemon} = this.props;
-      console.log(pokemon)
-      return (
-          <div style={headerStyle}>
-                  <p className="title"><a href="/"><img src="https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png" alt="PokeDex Logo" /></a></p>
-                  <ul>
-                      {pokemon.map((pokemon, id) => {
-                      return (
-                          <li style= {list}>
-                              <a href={`/pokemon/${id}`}> {pokemon.name}</a>
-                          </li>
-                      );
-                  })}
-                  </ul>
-          </div>
-      )
+    };
+// List Styling
+  const listingStyle = {
+        listStyle: 'none',
+        borderRadius: '4px',
+        textAlign: 'center',
+        textDecoration: 'none'
+    };
+// ListItem Styling
+    const listItemStyle = {
+        textAlign: 'center',
+        textDecoration: 'none',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        gridGap: '10px'
+    };
+    const editor = {
+        fontFamily: 'Times new roman',
+        fontSize: '16px',
+        textDecoration: 'none',
+        color: 'white'
     }
+
+    
+    
+export default class Index extends Component {
+  render() {
+    const {Pokemon} = this.props;
+    console.log(Pokemon)
+    return (
+    <html style={htmlStyling}>
+        <body style={bodyStyling}>
+        <nav style={navStyling}>
+                <ul style={listingStyle}>
+                    <li style={listItemStyle}><a href="/new" className="newButton"> Create New Pokemon</a></li>
+                </ul>
+            </nav>
+            <div style={myStyle}>
+                    <p className="title"><a href="/"><img src="https://i.ibb.co/wRQH3Gh/Pokedex-logo.png" alt="PokeDex Logo" /></a></p>
+                    <ul style={listingStyle}>
+                        {Pokemon.map((Pokemon) => {
+                        return (
+                            <li style={listItemStyle}>
+                                <a href={`/pokemon/${Pokemon.id}`}> {Pokemon.name}</a>
+                                <a style={editor} href={`/pokemon/${Pokemon.id}/edit`} className="editor">Edit {Pokemon.name}</a>
+                                <form action={`/pokemon/${Pokemon.id}?_method=DELETE`} method="POST">
+                                    <input type="submit" value="Remove Pokemon"/>
+                                </form>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </body>
+    </html>
+    )
   }
+}
